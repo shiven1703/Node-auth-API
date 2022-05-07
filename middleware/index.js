1249155de014cee7c7ec6cf71a1341a5a36b6b35
shiven1703 <shiven1703@gmail.com>
2022-05-07 17:23:53 +0200

    added helmet

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const helmet = require('helmet')
 
 module.exports = (app) => {
+  app.use(helmet())
+
   app.use(cors())
 
   app.use(express.json())
